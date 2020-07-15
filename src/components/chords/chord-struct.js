0c6a6aa9c2aa8f7eefb5ba8e0d027c5aaa2e3243
Michael Peterson <michaeljoepeterson@gmail.com>
2020-07-14 20:21:28 -0600

    setup styling for chord diagram background

--- src/components/chords/chord-struct.js
+++ src/components/chords/chord-struct.js
@@ -1,4 +1,15 @@
-import chordDiagram from '../../assets/chords/chord_horizontal.png'
+import chordDiagram from '../../assets/chords/chord_horizontal.png';
+import firstFinger from '../../assets/chords/1-finger.png';
+import secondFinger from '../../assets/chords/2-finger.png';
+import thirdFinger from '../../assets/chords/3-finger.png';
+import fourthFinger from '../../assets/chords/4-finger.png';
+import thumb from '../../assets/chords/thumb.png';
+import muted from '../../assets/chords/muted.png';
+import barre2 from '../../assets/chords/2-string-barre.png';
+import barre3 from '../../assets/chords/3-string-barre.png';
+import barre4 from '../../assets/chords/4-string-barre.png';
+import barre5 from '../../assets/chords/5-string-barre.png';
+import barre6 from '../../assets/chords/6-string-barre.png';
 
 export default class ChordStruct{
     name = null;
@@ -7,7 +18,18 @@ export default class ChordStruct{
     notePositions = [];
 
     chordImageMap = {
-        chordDiagram
+        chordDiagram,
+        firstFinger,
+        secondFinger,
+        thirdFinger,
+        fourthFinger,
+        thumb,
+        muted,
+        barre2,
+        barre3,
+        barre4,
+        barre5,
+        barre6
     };
 
     constructor(options){
@@ -18,6 +40,62 @@ export default class ChordStruct{
             this.notePositions = options.notePositions ? options.notePositions : null;
         }
     }
+
+    testChord = {
+        "name":"C Major",
+        "notes":["c","e","g"],
+        "degrees":[1,3,5],
+        "notePositions":[
+            {
+                "image":"",
+                "finger":0,
+                "fret":0,
+                "strings":[6],
+                "note":"e",
+                "degree":0
+            },
+            {
+                "image":"thirdFinger",
+                "finger":3,
+                "fret":3,
+                "strings":[5],
+                "note":"c",
+                "degree":1
+            },
+            {
+                "image":"secondFinger",
+                "finger":2,
+                "fret":2,
+                "strings":[4],
+                "note":"e",
+                "degree":3
+            },
+            {
+                "image":"",
+                "finger":0,
+                "fret":0,
+                "strings":[3],
+                "note":"g",
+                "degree":5
+            },
+            {
+                "image":"firstFinger",
+                "finger":1,
+                "fret":1,
+                "strings":[2],
+                "note":"c",
+                "degree":1
+            },
+            {
+                "image":"",
+                "finger":0,
+                "fret":0,
+                "strings":[1],
+                "note":"e",
+                "degree":3
+            }
+        ]
+    };
 
 }
 
--- src/components/chords/chord.js
+++ src/components/chords/chord.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ChordStruct from './chord-struct';
+import './styles/chord.css'
 
 export class Chord extends React.Component {
   constructor(props) {
@@ -10,12 +11,29 @@ export class Chord extends React.Component {
         chord
     };
   }
+  
+  componentDidMount = () => {
+    const testChord = 'C Major';
+    if(this.props.chord || testChord){
+        this.getChord();
+    }
+      
+  }
+
+  getChord = (chordName) => {
+    let testChord = new ChordStruct(this.state.chord.testChord);
+    this.setState({
+        chord:testChord
+    });
+  }
+
   render(){
-    console.log(this.state.chord)
+    
+    console.log(this.state.chord);
     return (
-        <div className="chord">
+        <div className="chord" style={{backgroundImage:`url(${this.state.chord.chordImageMap.chordDiagram})`}}>
             <p>chord</p>
-            <img src={this.state.chord.chordImageMap.chordDiagram} alt="chord diagram"/>
+            {/* <img src={this.state.chord.chordImageMap.chordDiagram} alt="chord diagram"/> */}
         </div>
     );
   }
