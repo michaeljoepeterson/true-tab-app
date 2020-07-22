8fe20f80b148f11993abec4fa2d0ad045a41a7a2
Michael Peterson <michaeljoepeterson@gmail.com>
2020-07-21 19:55:11 -0600

    first working display for desktop for dynamic generation of fret board

---
+++ src/components/note-checker.js
@@ -0,0 +1,11 @@
+export default class NoteChecker{
+    notes = ['A','A#/Bb','B','C','C#/Db','D','D#/Eb','E','F','F#/Gb','G','G#','Ab'];
+
+    constructor(){
+
+    }
+
+    checkNote(string,fret){
+        
+    }
+}
